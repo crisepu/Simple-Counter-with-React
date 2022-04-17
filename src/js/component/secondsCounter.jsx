2664f7562d60c9793e766b5015d24c240f723cb2
crisepu <csepulvedavenegas@gmail.com>
2022-04-17 02:28:34 +0000

    logic ready!

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import PropType from "prop-types";
 
 const SecondsCounter = (props) => {
-
-    
 	return (
 		<div className="container">
 			<div className="row">
 				<div className="col-md-12">
-					<i className="fa-regular fa-hourglass"></i>
-					<span>{props.digit}</span>
+					<span>
+						<i className="fa-regular fa-hourglass"></i>
+					</span>
+					<span>{props.milCen}</span>
+					<span>{props.milDec}</span>
+					<span>{props.mil}</span>
+					<span>{props.cen}</span>
+					<span>{props.dec}</span>
+					<span>{props.un}</span>
 				</div>
 			</div>
 		</div>
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,49 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import SecondsCounter from "./component/secondsCounter.jsx";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+let un = 0;
+let dec = 0;
+let cen = 0;
+let mil = 0;
+let milDec = 0;
+let milCen = 0;
 
-//render your react application
+setInterval(function () {
+	if (un > 9) {
+		dec = dec + 1;
+		un = 0;
+	}
+	if (dec > 9) {
+		cen = cen + 1;
+		dec = 0;
+	}
+	if (cen > 9) {
+		mil = mil + 1;
+		cen = 0;
+	}
+	if (mil > 9) {
+		milDec = milDec + 1;
+		mil = 0;
+	}
+	if (milDec > 9) {
+		milCen = milCen + 1;
+		milDec = 0;
+	}
 
-ReactDOM.render(<Home />, document.querySelector("#app"));
+	console.log(dec);
+	ReactDOM.render(
+		<SecondsCounter
+			un={un}
+			dec={dec}
+			cen={cen}
+			mil={mil}
+			milDec={milDec}
+			milCen={milCen}
+		/>,
+		document.querySelector("#app")
+	);
+	un = un + 1;
+}, 1000);
